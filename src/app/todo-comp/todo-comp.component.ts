8cc085ac4411a07d8c31fb95878f5e50f7c8db75
Daniel Ostrovsky <danduh@gmail.com>
2018-07-22 15:54:00 +0300

    Remove nested node_modules folder (./server/node_modules) And added to .gitignore
    minor fixes :)

--- src/app/todo-comp/todo-comp.component.ts
+++ src/app/todo-comp/todo-comp.component.ts
@@ -1,120 +1,106 @@
-import { Component, OnInit } from '@angular/core';
-import { BaseTodo, Todo } from '../todo'
-import { TodoService } from '../todo.service'
+import {Component, OnInit} from '@angular/core';
+import {BaseTodo, Todo} from '../todo';
+import {TodoService} from '../todo.service';
 
 
 @Component({
-  selector: 'app-todo-comp',
-  templateUrl: './todo-comp.component.html',
-  styleUrls: ['./todo-comp.component.css']
+    selector: 'app-todo-comp',
+    templateUrl: './todo-comp.component.html',
+    styleUrls: ['./todo-comp.component.css']
 })
 
 export class TodoCompComponent implements OnInit {
-  todoList: Todo[];
-  readonly filterOptions = ["ALL", "ACTIVE", "COMPLETED"];
-  currentFilter = "ALL";
-  //subscrEdit:any;
+    todoList: Todo[];
+    readonly filterOptions = ['ALL', 'ACTIVE', 'COMPLETED'];
+    currentFilter = 'ALL';
 
-
-  constructor(private todoService: TodoService) { }
-
-  addTodo(newTodoVal: string): void {
-    if(newTodoVal) {
-      const _todo = { name: newTodoVal };
-      this.todoService.saveTodo(_todo)
-      .subscribe( (todo) => {
-          this.todoList.push(todo)
-      });
-    }
-  }
-
-  removeTodo(id: number): void {
-    this.todoService.deleteTodo(id)
-    .subscribe( _ => {
-        this.todoList.splice(this.findIndexById(id), 1)});
+    constructor(private todoService: TodoService) {
     }
 
-  openInputForEdit(event: any): void {
-    let divToHide = event.currentTarget.parentElement;
-    let inputToShow = divToHide.closest("li").querySelector("input.hide");
-    divToHide.classList.add("hide");
-    inputToShow.classList.add("edit");
-    inputToShow.focus();
-
-}
-  editTodo(event: any, id: number, newName: string): void {
-    // if(this.subscrEdit) {             it was an attempt to unsubscribe, but it didn't work correctly
-    //   this.subscrEdit.unsubscribe();
-    // }
-    let curTarget = event.currentTarget;
-    if (newName == "") {
-      this.removeTodo(id);
-    } else {
-    //  this.subscrEdit =
-      this.todoService.updateTodo(id, newName)
-      .subscribe((_id) => {
-        this.todoList[this.findIndexById(_id)].name = newName;
-        curTarget.classList.remove("edit");
-        curTarget.closest("li").querySelector("div.hide").classList.remove("hide");
-      })
+    addTodo(newTodoVal: string): void {
+        if (newTodoVal) {
+            const _todo = {
+                name: newTodoVal,
+                completed: false
+            };
+            this.todoService.saveTodo(_todo)
+                .subscribe((todo) => {
+                    this.todoList.push(todo);
+                });
+        }
     }
-}
+
+    removeTodo(id: number): void {
+        this.todoService.deleteTodo(id)
+            .subscribe(_ => {
+                this.todoList.splice(this.findIndexById(id), 1);
+            });
+    }
 
 
-completeToggle(id: number, isChecked: boolean):void {
-  this.todoService.updateTodo(id, "", isChecked)
-  .subscribe((_id) => {
-    this.todoList[this.findIndexById(_id)].completed = isChecked;
-
-  })
-}
-
-
-matchFilter(todo: Todo):boolean {
-  if(this.currentFilter === "ALL" ||
-  (this.currentFilter === "COMPLETED" && todo.completed === true) ||
-  (this.currentFilter === "ACTIVE" && todo.completed === false)) {
-    return true;
-  }
-  return false;
-}
-
-clearCompleted():void {
-  this.todoService.removeAllCompleted()
-  .subscribe((todos) => {this.todoList = todos});
-}
+    editTodo(todo): void {
+        if (!!todo.name) {
+            this.todoService.updateTodo(todo.id, todo)
+                .subscribe(() => {
+                });
+        } else {
+            this.removeTodo(todo.id);
+        }
+    }
 
 
- private countByCompleted(completed:boolean):number {
-   let counter=0;
-   this.todoList.forEach((item) => {
-     if(item.completed ===completed) {
-       counter++;
-     }
-   })
-   return counter;
-  }
+    completeToggle(todo): void {
+        todo.completed = !todo.completed;
+        this.todoService.updateTodo(todo.id, todo)
+            .subscribe((_id) => {
+            });
+    }
 
-  private findIndexById(id: number): number {
-    let indexOfTodo = -1;
-    this.todoList.forEach((item, i) => {
-      if (item.id === id) {
-        indexOfTodo = i;
-      }
-    });
-    return indexOfTodo;
-  }
+    clearCompleted(): void {
+        const ids = this.todoList
+            .filter(todo => todo.completed)
+            .map(todo => todo.id);
 
-  get activeCount():number {
-    return this.countByCompleted(false);
-  }
-  get completedCount():number {
-    return this.countByCompleted(true);
-  }
+        this.todoService.removeAllCompleted(ids)
+            .subscribe((todos) => {
+                this.todoList = todos;
+            });
+    }
 
-  ngOnInit() {
-    this.todoService.getTodoList()
-    .subscribe((todoFromStorage) => {this.todoList = todoFromStorage});
-  }
+
+    private countByCompleted(completed: boolean): number {
+        let counter = 0;
+        this.todoList.forEach((item) => {
+            if (item.completed === completed) {
+                counter++;
+            }
+        });
+        return counter;
+    }
+
+    private findIndexById(id: number): number {
+        let indexOfTodo = -1;
+        this.todoList.forEach((item, i) => {
+            if (item.id === id) {
+                indexOfTodo = i;
+            }
+        });
+        return indexOfTodo;
+    }
+
+    get activeCount(): number {
+        return this.countByCompleted(false);
+    }
+
+    get completedCount(): number {
+        return this.countByCompleted(true);
+    }
+
+    ngOnInit() {
+        this.todoService.getTodoList()
+            .subscribe((todoFromStorage) => {
+                this.todoList = todoFromStorage;
+            });
+    }
 
 }
